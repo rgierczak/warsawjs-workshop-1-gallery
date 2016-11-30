969d3464489aff07f760d6cc95cb9a1abd8aa27c
Rafał Gierczak <grime85@gmail.com>
2016-11-30 09:47:01 +0100

    Setup listeners for clicked previous/next buttons, added assets

--- src/js/main.es6.js
+++ src/js/main.es6.js
@@ -1,6 +1,32 @@
 class Gallery {
     constructor() {
         console.log('Gallery');
+        this.setup();
+    }
+
+    setup() {
+        Gallery.addDOMContentLoaderListener(this.setupClickListeners.bind(this));
+    }
+
+    static addButtonListener($button, callback) {
+        document.getElementById($button).addEventListener("click", callback);
+    }
+
+    static addDOMContentLoaderListener(handler) {
+        document.addEventListener("DOMContentLoaded", handler);
+    }
+
+    setupClickListeners() {
+        Gallery.addButtonListener("nextButton", this.nextButtonHandler.bind(this));
+        Gallery.addButtonListener("previousButton", this.previousButtonHandler.bind(this));
+    }
+
+    nextButtonHandler() {
+        console.log('next clicked, context: ', this);
+    }
+
+    previousButtonHandler() {
+        console.log('previous clicked, context: ', this);
     }
 }
 
